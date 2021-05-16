26d6e14c02f1fd88405bc610b95eadb06e7bfba5
xushuoqiang <84257025+xushuoqiang@users.noreply.github.com>
2021-05-16 10:38:48 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("- E G E G E G - ", 500)
+})
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    . # # # .
+    . . # . .
+    `)
+basic.forever(function () {
+	
+})
